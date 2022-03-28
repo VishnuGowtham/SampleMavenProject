88fd0d21c647a0d87d5fe43b079aa6b93d10e79b
Admin <Admin@VishnuPC>
2022-03-28 15:05:33 +0530

    Added new EMAIL class

---
+++ com.crm.DemoVtiger/src/test/java/com/crm/VTiger/PratcieTestCases/CreateEmail.java
@@ -0,0 +1,14 @@
+package com.crm.VTiger.PratcieTestCases;
+
+import org.testng.annotations.Test;
+
+public class CreateEmail {
+
+	@Test
+	
+	public void createEmail() {
+		
+		System.out.println("Create a email");
+	}
+	
+}
