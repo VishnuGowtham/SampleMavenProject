07fc88756d72d5dc57cd197f3fbeda6b320709b6
Admin <Admin@VishnuPC>
2022-03-28 15:56:54 +0530

    get data class is added

---
+++ com.crm.DemoVtiger/src/test/java/com/crm/VTiger/PratcieTestCases/GetDataTest.java
@@ -0,0 +1,16 @@
+package com.crm.VTiger.PratcieTestCases;
+
+import org.testng.annotations.Test;
+
+public class GetDataTest {
+
+	@Test
+	public void getData() {
+		
+		System.out.println(System.getProperty("browser"));
+		System.out.println(System.getProperty("url"));
+		System.out.println(System.getProperty("username"));
+		System.out.println(System.getProperty("password"));
+		
+	}
+}
